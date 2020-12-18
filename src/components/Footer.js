1e4dce3ac5b50844d1ee4c3b70ea824f2037f503
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-12-18 12:17:32 -0500

    [components] add About and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () =< {
+  return (
+    // See Bootstrap CSS for this:
+    <footer className="footer bg-dark">
+      <div>
+        <span className="text-muted">@2020 Built by Andrew McManus</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
