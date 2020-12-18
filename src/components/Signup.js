89d2b50e91f922785963dda4a1a006820006ace0
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-12-18 16:26:52 -0500

    [signup] add handle functions in form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,27 +11,57 @@ const Signup = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
 
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      const newUser = { name, email, password };
+      // send this to our API
+      // does the POST request we've tried in Postman
+      axios.post(`${REACT_APP_SERVER_URL}/controllers/users/register`, newUser).then(response => {
+        console.log(response);
+        setRedirect(true);
+      }).catch(error = {
+        console.log(error);
+      })
+    }
+  }
+
+  if (redirect) return <Redirect to="/login" />
+
   return (
     <div className="row mt-4">
       <div className="col-md-7 offset-md-3">
         <div className="card card-body">
           <h2 className="py-2">Signup</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" value={name} onchange={} className="form-control" />
+              <input type="text" name="name" value={name} onchange={handleName} className="form-control" />
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="email" name="name" value={email} onchange={} className="form-control" />
+              <input type="email" name="name" value={email} onchange={handleEmail} className="form-control" />
             </div>
             <div className="form-group">
               <label htmlFor="password">password</label>
-              <input type="password" name="password" value={password} onchange={} className="form-control" />
+              <input type="password" name="password" value={password} onchange={handlePassword} className="form-control" />
             </div>
             <div className="form-group">
               <label htmlFor="confirmPassword">Confirm Password</label>
-              <input type="password" name="confirmPassword" value={confirmPassword} onchange={} className="form-control" />
+              <input type="password" name="confirmPassword" value={confirmPassword} onchange={handleConfirmPassword} className="form-control" />
             </div>
           </form>
         </div>
